22e5ce57f8540cf629f6fe138821daa4e2349e86
srbh001 <22B3905@iitb.ac.in>
2025-02-02 03:05:14 +0530

    stage 3

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ import sys
 def main():
     sys.stdout.write("$ ")
 
-    command = input()
-    print(f"{command}: command not found")
+    while True:
+        command = input()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
