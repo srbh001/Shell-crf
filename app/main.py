e49975146e12af622b14e592a5a959c1a9a9eaa2
srbh001 <22B3905@iitb.ac.in>
2025-02-02 03:11:17 +0530

    stage 4

--- app/main.py
+++ app/main.py
@@ -5,7 +5,16 @@ def main():
     sys.stdout.write("$ ")
 
     while True:
-        command = input()
+        inp = input().strip().split(" ")
+
+        command = inp[0]
+        code = None
+        if len(inp) > 1:
+            code = int(inp[1])
+
+        if command == "exit":
+            sys.exit(code)
+
         print(f"{command}: command not found")
         sys.stdout.write("$ ")
 
