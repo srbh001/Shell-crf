eff689ab74b51caa268f485fc3e2bc18b1e2421a
srbh001 <22B3905@iitb.ac.in>
2025-02-02 03:14:40 +0530

    stage 5

--- app/main.py
+++ app/main.py
@@ -9,13 +9,18 @@ def main():
 
         command = inp[0]
         code = None
-        if len(inp) > 1:
+        if len(inp) > 1 and command == "exit":
             code = int(inp[1])
 
         if command == "exit":
             sys.exit(code)
 
-        print(f"{command}: command not found")
+        elif command == "echo":
+            msg = " ".join(inp[1:])
+            sys.stdout.write(f"{msg}\n")
+
+        else:
+            print(f"{command}: command not found")
         sys.stdout.write("$ ")
 
 
