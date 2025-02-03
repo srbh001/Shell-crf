66931ef919703a7b62684da86458b87189a4df89
srbh001 <22B3905@iitb.ac.in>
2025-02-03 08:43:37 +0530

    quotations

--- app/main.py
+++ app/main.py
@@ -109,7 +109,7 @@ def main():
 
             elif is_external_command(command):
 
-                subprocess.run([command, args])
+                subprocess.run([inp])
 
             else:
                 print(f"{command}: command not found")
